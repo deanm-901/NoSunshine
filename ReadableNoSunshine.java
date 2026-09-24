ecd220e4894f738377995d087eefa9cb1a2133f2
shidfardfard <nearlydrawing@gmail.com>
2026-09-24 09:00:29 -0700

    627 Characters

--- ReadableNoSunshine.java
+++ ReadableNoSunshine.java
@@ -1,16 +1,17 @@
 class ReadableNoSunshine {
-    String sg="she's gone",
-    iK="I know",
-    ik=iK+", ",
-    ns="no sunshine",
-    hm="And this house just ain't no home",
-    at="Anytime she goes away",
+    String s="she's gone",
+    I="I know",
+    i=I+", ",
+    n="no sunshine",
+    h="And this house just ain't no home",
+    t="Anytime she goes away",
     ai="Ain't ",
-    an="And ",
-    wo="Wonder ",
-    a=ai+ns+" when "+sg;
+    d="And ",
+    w="Wonder ",
+    l="\n",
+    a=ai+n+" when "+s;
     void p(String...s){
-        for(String x:s)System.out.println(x);
+        for(var x:s)System.out.println(x);
     }
     String m(String s,int m){
         return s.repeat(m);
@@ -19,22 +20,22 @@ class ReadableNoSunshine {
         p(a,
         "It's not warm when she's away",
         a,
-        an+"she's always gone too long",
-        at+"\n",
-        wo+"this time where "+sg,
-        wo+"if she's gone to stay",
+        d+"she's always gone too long",
+        t+l,
+        w+"this time where "+s,
+        w+"if she's gone to stay",
         a,
-        hm,
-        at+"\n",
-        an+m(ik,3)+iK,
-        m(m(ik,4)+iK+"\n",3)+m(ik,4)+iK,
-        ik+iK+"\n",
+        h,
+        t+l,
+        d+m(i,3)+I,
+        m(m(i,4)+I+l,3)+m(i,4)+I,
+        i+I+l,
         "Hey, I ought to leave young thing alone",
-        "But ain't "+ns+" when "+sg+", whoa-whoa",
+        "But ain't "+n+" when "+s+", whoa-whoa",
         a,
         "Only darkness every day\n",
         a,
-        hm+"\n",
-        m(at+"\n",4));
+        h+l,
+        m(t+l,4));
     }
 }
